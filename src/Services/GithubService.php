cfde0ae1edf02adfa38e06751f331a531b0b0b29
Niels Hamelink <niels@gyvex.com>
2025-04-30 11:44:27 +0200

    Implemented Github File package & upgraded version

--- src/Services/GithubService.php
+++ src/Services/GithubService.php
@@ -4,6 +4,7 @@ namespace Veiliglanceren\LaravelRemoteDocumentation\Services;
 
 use Exception;
 use Illuminate\Support\Facades\Http;
+use VeiligLanceren\GithubFile\Facades\GithubFile;
 use Veiliglanceren\LaravelRemoteDocumentation\Exceptions\FetchDocumentationException;
 use Veiliglanceren\LaravelRemoteDocumentation\Interfaces\Services\IGithubService;
 
@@ -15,15 +16,9 @@ class GithubService implements IGithubService
     public function fetch(string $repo, string $file = 'README.md'): string
     {
         try {
-            $response = Http::get("https://raw.githubusercontent.com/{$repo}/main/{$file}");
-
-            if ($response->failed()) {
-                throw new FetchDocumentationException("Failed to fetch file {$file} from {$repo}.");
-            }
-
-            return $response->body();
-        } catch (\Throwable $e) {
-            throw new FetchDocumentationException($e->getMessage(), $e->getCode(), $e);
+            return GithubFile::get($repo, $file);
+        } catch (Exception $exception) {
+            throw new FetchDocumentationException($exception->getMessage());
         }
     }
 }
